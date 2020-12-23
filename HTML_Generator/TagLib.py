6c317435adedaa3bc0569d88695a0553a62f89a8
Zohic <70917733+Zohic@users.noreply.github.com>
2020-12-23 09:49:24 +0300

    Add files via upload

--- HTML_Generator/TagLib.py
+++ HTML_Generator/TagLib.py
@@ -3,44 +3,38 @@
 
 class Tag:
     def __init__(self):
-        self.tagStr = "tagName"
-        self.innerHTML = ""
+        self.tagName = "tagName"
+        self.text = ""
         self.attributes = ""
-        self.container = []
+        self.inside = []
         self.single = True
         self.parent = None
         self.localPos = -1
-        self.begin = -1
-        self.end = -1
 
-    def Generate(self):
-        tabs = -1
-        ptag = self.parent
-        while not (ptag==None):
-            ptag = ptag.parent
-            tabs+=1
+    def GenerateString(self,tabs):
+
         tab = "\t"*tabs
 
-        ostr = tab+"<"+self.tagStr+" "+self.attributes+">"+self.innerHTML
-        if len(self.container)>0:
-            ostr=ostr+"\n"
+        outString = tab+"<"+self.tagName+" "+self.attributes+">"+self.text
+        if len(self.inside)>0:
+            outString=outString+"\n"
 
-        for tag in self.container:
-            ostr = ostr+tag.Generate()
+        for tag in self.inside:
+            outString = outString+tag.GenerateString(tabs+1)
    
         if self.single:
-            ostr = ostr+"\n"
+            outString = outString+"\n"
         else:
-            if len(self.container)>0:
-                ostr = ostr+tab+"</"+self.tagStr+"> \n"
+            if len(self.inside)>0:
+                outString = outString+tab+"</"+self.tagName+"> \n"
             else:
-                ostr = ostr+"</"+self.tagStr+"> \n"
+                outString = outString+"</"+self.tagName+"> \n"
 
-        return ostr
+        return outString
 
     def GenerateFile(self, path):
         file = open(path, "w")
-        file.write(self.Generate())
+        file.write(self.GenerateString(0))
         file.close
 
     pass
@@ -49,7 +43,7 @@ class Tag:
 class Img(Tag):
     def __init__(self, imagePath, attr=""):
         Tag.__init__(self)
-        self.tagStr = "img"
+        self.tagName = "img"
         self.attributes = "src='"+imagePath+"' "+attr
         self.single = True
     
@@ -57,14 +51,14 @@ class Img(Tag):
 class Br(Tag):
     def __init__(self):
         Tag.__init__(self)
-        self.tagStr = "br"
+        self.tagName = "br"
         self.single = True
 
 class Script(Tag):
     def __init__(self, text):
         Tag.__init__(self)
-        self.tagStr = "script"
-        self.innerHTML = text
+        self.tagName = "script"
+        self.text = text
         self.attributes="type='text/javascript'"
         self.single = False
     
@@ -72,58 +66,58 @@ class Script(Tag):
 class Style(Tag):
     def __init__(self, text):
         Tag.__init__(self)
-        self.tagStr = "style"
-        self.innerHTML = text
+        self.tagName = "style"
+        self.text = text
         self.attributes="type='text/css'"
         self.single = False
 
 
-class tagP(Tag):
+class P(Tag):
     def __init__(self,text,atr=""):
         Tag.__init__(self)
-        self.tagStr = "p"
-        self.innerHTML = text
+        self.tagName = "p"
+        self.text = text
         self.attributes = atr
         self.single=False
 
-class tagA(Tag):
+class A(Tag):
     def __init__(self,text,atr=""):
         Tag.__init__(self)
-        self.tagStr = "a"
-        self.innerHTML = text
+        self.tagName = "a"
+        self.text = text
         self.attributes = atr
         self.single=False
 
-class tagH(Tag):
+class H(Tag):
     def __init__(self,size,text,atr=""):
         Tag.__init__(self)
         if size<1:
             size=1
         if size>6:
             size=6
-        self.tagStr = "h"+str(size)
-        self.innerHTML = text
+        self.tagName = "h"+str(size)
+        self.text = text
         self.attributes = atr
         self.single=False
     
 class Button(Tag):
     def __init__(self,text,atr):
         Tag.__init__(self)
-        self.tagStr = "button"
-        self.innerHTML = text
+        self.tagName = "button"
+        self.text = text
         self.attributes = atr
         self.single=False
 
 class Input(Tag):
     def __init__(self,placeholder=""):
         Tag.__init__(self)
-        self.tagStr = "input"
+        self.tagName = "input"
         self.attributes = "placeholder='"+placeholder+"' "
         self.single=True
 
 class Meta(Tag):
     def __init__(self,atr):
-        self.tagStr = "meta"
+        self.tagName = "meta"
         Tag.__init__(self)
         self.single = True
         self.attributes = atr
@@ -131,52 +125,52 @@ class Meta(Tag):
 class Link(Tag):
     def __init__(self, text, link):
         Tag.__init__(self)
-        self.tagStr = "a"
-        self.innerHTML = text
+        self.tagName = "a"
+        self.text = text
         self.attributes = "href='"+link+"'"
         self.single=False
 
 class Title(Tag):
     def __init__(self,text):
         Tag.__init__(self)
-        self.tagStr = "title"
-        self.innerHTML = text
+        self.tagName = "title"
+        self.text = text
         self.single=False
     
 
-class ContainerTag(Tag):
+class InsideTag(Tag):
     def __init__(self, data):
         Tag.__init__(self)
-        self.container = data
+        self.inside = data
         self.single = False
-        for i in range(len(self.container)):
-            self.container[i].parent = self
-            self.container[i].localPos = i
+        for i in range(len(self.inside)):
+            self.inside[i].parent = self
+            self.inside[i].localPos = i
     pass
 
-class Html(ContainerTag):
+class Html(InsideTag):
     def __init__(self, data):
-        ContainerTag.__init__(self,data)
-        self.tagStr = "html"
+        InsideTag.__init__(self,data)
+        self.tagName = "html"
     
 
-class Div(ContainerTag):
+class Div(InsideTag):
     def __init__(self, data, attr=""):
-        ContainerTag.__init__(self,data)
-        self.tagStr = "div"
+        InsideTag.__init__(self,data)
+        self.tagName = "div"
         self.attributes = attr
 
-class Head(ContainerTag):
+class Head(InsideTag):
     def __init__(self, data):
-        ContainerTag.__init__(self,data)
-        self.tagStr = "head"
+        InsideTag.__init__(self,data)
+        self.tagName = "head"
        
     
 
-class Body(ContainerTag):
+class Body(InsideTag):
     def __init__(self, data,attr=""):
-        ContainerTag.__init__(self,data)
-        self.tagStr = "body"
+        InsideTag.__init__(self,data)
+        self.tagName = "body"
         self.attributes=attr
    
 
@@ -193,21 +187,21 @@ examplePage = Html([
                 Div([
                     Div([
                             Img("https://pa1.narvii.com/7602/069f9aa24527d326b9987b4efa11abacbcf50200r1-480-384_128.gif","style='width:20%; height:80%'"),
-                            tagA("WELL ", "style=' font-size:180%; font-family:fantasy; color:white'"),
+                            A("WELL ", "style=' font-size:180%; font-family:fantasy; color:white'"),
                             Img("https://sun9-8.userapi.com/impf/pZ8KhmmiETJNf4bHtAPYdUke_f5qgY4ahCQeAg/EYvdPbN0rxQ.jpg?size=756x484&quality=96&proxy=1&sign=f885aad1f633bba5dfaab18600e37908&type=album","style='width:50%;'"),
-                            tagA(" COME","style='font-size:180%; font-family:fantasy; color:white'"),
+                            A(" COME","style='font-size:180%; font-family:fantasy; color:white'"),
                             Img("https://pa1.narvii.com/7602/069f9aa24527d326b9987b4efa11abacbcf50200r1-480-384_128.gif","style='width:20%; height:80%'"),
                         ],"style='margin-left:0%;'"),
-                        tagA("to my site", "style='font-size:180%; font-family:fantasy; color:white; margin-left:45%'")
+                        A("to my site", "style='font-size:180%; font-family:fantasy; color:white; margin-left:45%'")
                     ]),
                 Div([
                         Div([
-                            tagP("Обо мне","class='info'"),
-                            tagP("Назиров Захар:","class='info'"),
-                            tagP("3431101/00001","class='info'")
+                            P("Обо мне","class='info'"),
+                            P("Назиров Захар:","class='info'"),
+                            P("3431101/00001","class='info'")
                         ],"class='info' style='white-space: nowrap'")
                     ])
             ],"bgcolor='black'")
     ])
 
-examplePage.GenerateFile("out.html")
+examplePage.GenerateFile("exell.html")
