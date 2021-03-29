1d221da0136c82c4cc8b281a627fe87e56400383
Zohic <70917733+Zohic@users.noreply.github.com>
2021-03-29 13:48:56 +0300

    Create functions.h

---
+++ 21_lab/src/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int FindMin(int arr[], int len);
+int FindMax(int arr[], int len);
+int CountEnters(int arr[], int len, int tar);
+
+
+#endif // FUNCTIONS_H
